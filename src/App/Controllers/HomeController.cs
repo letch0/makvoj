32cf0fa3b209cbeb828c1fedc8a5176dd2f276e3
Maki <felisek.marketa@gmail.com>
2024-04-07 22:09:45 +0200

    gort_list_nacrt

--- src/App/Controllers/HomeController.cs
+++ src/App/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Results()
+    public IActionResult CloseUp()
     {
         return View();
     }
